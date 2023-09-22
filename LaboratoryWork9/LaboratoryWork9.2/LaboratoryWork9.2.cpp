7dfa8d922d46fa4f050634ef0c063ca702d87ef5
dimonkiss <dklxwa@gmail.com>
2023-09-22 19:24:02 +0300

    add 9 laby

---
+++ LaboratoryWork9/LaboratoryWork9.2/LaboratoryWork9.2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cctype>
+#include <locale.h>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    char firstString[100];
+    char secondString[100];
+
+    cout << "Введіть перший рядок символів: ";
+    cin.getline(firstString, sizeof(firstString));
+
+    cout << "Введіть другий рядок символів: ";
+    cin.getline(secondString, sizeof(secondString));
+
+    // Перевірка на однакову довжину
+    if (strlen(firstString) != strlen(secondString)) {
+        cout << "Помилка: Рядки мають різну довжину." << endl;
+        return 1;
+    }
+
+    // Обчислення ключа шифру
+    int key = tolower(secondString[0]) - tolower(firstString[0]);
+
+    // Зашифрування першого рядка
+    for (int i = 0; i < strlen(firstString); i++) {
+        if (isalpha(firstString[i])) {
+            char base = islower(firstString[i]) ? 'a' : 'A';
+            firstString[i] = ((firstString[i] - base + key + 26) % 26) + base;
+        }
+    }
+
+    // Порівняння зашифрованого рядка з другим рядком
+    if (strcmp(firstString, secondString) == 0) {
+        cout << "Другий рядок є шифрованим першого з ключем " << key << "." << endl;
+    }
+    else {
+        cout << "Другий рядок не є шифрованим першого." << endl;
+    }
+
+    return 0;
+}
---
+++ LaboratoryWork9/LaboratoryWork9/LaboratoryWork9.cpp
@@ -0,0 +1,72 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <locale.h>
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    char input[1000];  // Максимальна довжина рядка
+    printf("Введіть рядок: ");
+    fgets(input, sizeof(input), stdin);  // Отримуємо рядок від користувача (увага: ця функція не є безпечною для використання)
+
+    // Змінюємо рядок на рядок в нижньому регістрі для порівняння
+    for (int i = 0; input[i]; i++) {
+        input[i] = tolower(input[i]);
+    }
+
+    // Масив ключових слів
+    const char* keywords[] = { "if", "else", "switch", "while", "for", "do", "return", /* додайте інші ключові слова тут */};
+    int numKeywords = sizeof(keywords) / sizeof(keywords[0]);
+
+    // Масив для зберігання кількості входжень для кожного ключового слова
+    int* keywordCounts = new int[numKeywords];
+    for (int i = 0; i < numKeywords; i++) {
+        keywordCounts[i] = 0;
+    }
+
+    // Зберігаємо знайдені букви для побудови ключового слова
+    char foundLetters[1000] = "";
+    int foundLetterIndex = 0;
+
+    // Шукаємо ключові слова та підраховуємо кількість входжень
+    for (int i = 0; i < numKeywords; i++) {
+        const char* keyword = keywords[i];
+        char* pos = strstr((char*)input, keyword);
+        size_t keywordLength = strlen(keyword);
+        while (pos != NULL) {
+            keywordCounts[i]++;
+
+            // Додаємо знайдені букви до списку
+            for (int j = 0; j < keywordLength; j++) {
+                if (strchr(foundLetters, keyword[j]) == NULL) {
+                    foundLetters[foundLetterIndex++] = keyword[j];
+                }
+            }
+
+            pos = strstr(pos + keywordLength, keyword);
+        }
+    }
+
+    // Визначаємо, чи було знайдено хоча б одне ключове слово
+    int totalKeywords = 0;
+    for (int i = 0; i < numKeywords; i++) {
+        totalKeywords += keywordCounts[i];
+    }
+
+    // Якщо не було знайдено жодного ключового слова, виводимо зібрані букви
+    if (totalKeywords == 0) {
+        printf("Складене ключове слово: %s\n", foundLetters);
+    }
+    else {
+        // Виводимо кількість входжень для кожного ключового слова
+        for (int i = 0; i < numKeywords; i++) {
+            printf("Кількість входжень слова(з виділенням з букв рядка) '%s': %d\n", keywords[i], keywordCounts[i]);
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
